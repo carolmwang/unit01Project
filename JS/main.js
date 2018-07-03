53dd723addf0ac3bef5165032a7778958fb9edd0
Carol Wang <carolmwang@gmail.com>
2018-07-03 16:41:07 -0400

    html, css, and div-gameboard created

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+// 900px board, 3px wide, 30 cells
+
+let imageBoard = document.querySelector('.game-board');
+for (let i = 0; i < 90; i += 1) {
+    let gameCell = document.createElement('div');
+    gameCell.className = 'cell';
+    imageBoard.appendChild(gameCell);
+    }
+
+
+let 
+
+
+
