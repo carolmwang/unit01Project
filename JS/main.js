01fc8fdddea1d5579f1424a99137516be28e375e
Carol Wang <carolmwang@gmail.com>
2018-07-03 19:21:37 -0400

    added data attributes to cells

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,12 @@
 // 900px board, 3px wide, 30 cells
 
-let imageBoard = document.querySelector('.game-board');
-for (let i = 0; i < 90; i += 1) {
+let gameBoard = document.querySelector('.game-board');
+gameBoard.addEventListener('click', (e)=> console.log(e.target, e.currentTarget))
+for (let i = 0; i < 91; i += 1) {
     let gameCell = document.createElement('div');
     gameCell.className = 'cell';
-    imageBoard.appendChild(gameCell);
+    gameBoard.appendChild(gameCell);
+    gameCell.dataset.row = i;
     }
-
-
-let 
-
 
 
