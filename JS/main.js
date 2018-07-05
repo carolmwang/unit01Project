0a03473470bcf30ec6f2034ba4021f19921d69e3
Carol Wang <carolmwang@gmail.com>
2018-07-05 10:46:25 -0400

    added an img array and randomized 5

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,8 @@
-// 900px board, 3px wide, 30 cells
+// 5 images need to be randomized around the board
+// try to get the board cell to flip back to white if the image is not there
+// add remaining moves to html, css
+// decrease the remaining moves by 1 
+// work on hints 
 
 let gameBoard = document.querySelector('.game-board');
 gameBoard.addEventListener('click', (e)=> (e.target, e.currentTarget));
@@ -15,3 +19,14 @@ for (let i = 0; i < 91; i += 1) {
     cell[i].addEventListener('click', function(e) {
     cell[i].style.backgroundColor = '#838383';
 })}
+
+
+const allImages = ['../img/img1.jpeg','../img/img2.jpeg', '../img/img3.jpeg', '../img/img4.jpeg', '../img/img5.jpeg', '../img/img6.jpeg', '../img/img7.jpeg', '../img/img8.jpeg', '../img/img9.jpeg', '../img/img10.jpeg', '../img/img11.jpeg', '../img/img12.jpeg', '../img/img13.jpeg', '../img/img14.jpeg', '../img/img15.jpeg']
+
+
+let gameImages = [];
+for (let i = 0; i < 5; i += 1) {
+    gameImages.push(allImages[Math.floor(Math.random()*allImages.length)]);
+}
+// need to remove from allImages so there are no repeats
+
