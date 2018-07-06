775208cb50a8f894a0ff9e1b6c95a1b2751a5434
Carol Wang <carolmwang@gmail.com>
2018-07-05 20:16:58 -0400

    added images on click and added opacity to side images when found

--- JS/main.js
+++ JS/main.js
@@ -1,32 +1,83 @@
-// 5 images need to be randomized around the board
 // try to get the board cell to flip back to white if the image is not there
 // add remaining moves to html, css
 // decrease the remaining moves by 1 
 // work on hints 
 
+
+
+// array of all images
+const allImages = ['img/img1.jpeg','img/img2.jpeg', 'img/img3.jpeg', 'img/img4.jpeg', 'img/img5.jpeg', 'img/img6.jpeg', 'img/img7.jpeg', 'img/img8.jpeg', 'img/img9.jpeg', 'img/img10.jpeg', 'img/img11.jpeg', 'img/img12.jpeg', 'img/img13.jpeg', 'img/img14.jpeg', 'img/img15.jpeg']
+
+// random 5 images from all images array
+let gameImages = [];
+for (let i = 0; i < 5; i += 1) {
+    let arrayIndex = Math.floor(Math.random() * allImages.length)
+    let imagePush = allImages[arrayIndex];
+    allImages.splice(arrayIndex, 1);
+    gameImages.push(imagePush);
+}
+
+
+
+// create cells in gameboard
 let gameBoard = document.querySelector('.game-board');
-gameBoard.addEventListener('click', (e)=> (e.target, e.currentTarget));
 for (let i = 0; i < 91; i += 1) {
     let gameCell = document.createElement('div');
     gameCell.className = 'cell';
     gameBoard.appendChild(gameCell);
-    gameCell.dataset.row = i;
+    gameCell.dataset.name = i;
+    let imgEl = document.createElement('img');
+    gameCell.appendChild(imgEl);
     }
 
-
+// add event listeners to each cell
 const cell = document.querySelectorAll('.cell');
-for (let i = 0; i < 91; i += 1) {
-    cell[i].addEventListener('click', function(e) {
-    cell[i].style.backgroundColor = '#838383';
-})}
+const img = document.querySelectorAll('img');
 
 
-const allImages = ['../img/img1.jpeg','../img/img2.jpeg', '../img/img3.jpeg', '../img/img4.jpeg', '../img/img5.jpeg', '../img/img6.jpeg', '../img/img7.jpeg', '../img/img8.jpeg', '../img/img9.jpeg', '../img/img10.jpeg', '../img/img11.jpeg', '../img/img12.jpeg', '../img/img13.jpeg', '../img/img14.jpeg', '../img/img15.jpeg']
-
-
-let gameImages = [];
-for (let i = 0; i < 5; i += 1) {
-    gameImages.push(allImages[Math.floor(Math.random()*allImages.length)]);
+//create a random array of 5 indices and add images to them
+let array = [];
+while(array.length < 5) {
+    let randomNumber = Math.floor(Math.random() * 91) + 1;
+    if(array.indexOf(randomNumber) > -1) continue;
+    array[array.length] = randomNumber;
 }
-// need to remove from allImages so there are no repeats
+
+for(let i = 0; i < array.length; i += 1) {
+const imgEl = img[array[i]]
+imgEl.src = gameImages[i]; 
+imgEl.dataset.img = 1;
+imgEl.style.width = '0px';
+imgEl.style.height = '0px';
+};
+
+// gameBoard.addEventListener('click', (e)=> console.log(e.target.dataset.img))
+const scoreEl = document.querySelector('score');
+let score = parseInt(scoreEl);
+
+gameBoard.addEventListener('click', (e)=> {
+    const imgTarget = e.target.lastChild;
+    if (imgTarget && imgTarget.dataset.img == 1) {
+        imgTarget.style.width = '65px';
+        imgTarget.style.height = '65px';
+        score = score.innerHTML - 1;
+        const imgEl = document.querySelectorAll('.images-search');
+        for (let i = 0; i < imgEl.length; i += 1)
+            if (imgEl[i].src === imgTarget.src) {
+                imgEl[i].style.opacity = '0.3';
+            }
+    } else {
+        e.target.style.background = '#838383';
+    }
+})
+
+// add images to the hidden images div
+
+const hiddenImages = document.querySelector('.images');
+for (let i = 0; i < 5; i += 1) {
+    const imgEl = document.createElement('img');
+    hiddenImages.appendChild(imgEl);
+    imgEl.className = 'images-search';
+    imgEl.src = gameImages[i];
+}
 
