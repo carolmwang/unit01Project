db92a215effe03ab0ca7ab9a084e72c0e58ffedb
Carol Wang <carolmwang@gmail.com>
2018-07-06 11:33:34 -0400

    added winner or loser function

--- JS/main.js
+++ JS/main.js
@@ -36,12 +36,12 @@ const img = document.querySelectorAll('img');
 
 
 //create a random array of 5 indices and add images to them
-let array = [];
-while(array.length < 5) {
-    let randomNumber = Math.floor(Math.random() * 91) + 1;
-    if(array.indexOf(randomNumber) > -1) continue;
-    array[array.length] = randomNumber;
-}
+let array = [0, 1, 2, 3, 4];
+// while(array.length < 5) {
+//     let randomNumber = Math.floor(Math.random() * 91) + 1;
+//     if(array.indexOf(randomNumber) > -1) continue;
+//     array[array.length] = randomNumber;
+// }
 
 for(let i = 0; i < array.length; i += 1) {
 const imgEl = img[array[i]]
@@ -54,7 +54,15 @@ imgEl.style.height = '0px';
 // gameBoard.addEventListener('click', (e)=> console.log(e.target.dataset.img))
 let scoreEl = document.querySelector('.score');
 let score = 25;
+let imgWinner = 0;
 // add winner or loser class to event listener
+const winner = function() {
+    if (score === 0) {
+        document.body.classList.add('.game-over');
+    } else if (imgWinner === 5 && score > 0) {
+        document.body.classList.add('.winner');
+}}
+
 gameBoard.addEventListener('click', (e)=> {
     score -= 1
     scoreEl.innerHTML = score;
@@ -62,16 +70,19 @@ gameBoard.addEventListener('click', (e)=> {
     if (imgTarget && imgTarget.dataset.img == 1) {
         imgTarget.style.width = '65px';
         imgTarget.style.height = '65px';
-        // if (score === 0) document.body.classList.add('.new-game');
+        imgWinner += 1;
         const imgEl = document.querySelectorAll('.images-search');
         for (let i = 0; i < imgEl.length; i += 1)
-            if (imgEl[i].src === imgTarget.src) {
-                imgEl[i].style.opacity = '0.3';
-            }
+        if (imgEl[i].src === imgTarget.src) {
+            imgEl[i].style.opacity = '0.3';
+        }
     } else {
         e.target.style.background = '#838383';
     }
+    winner();
 })
+
+
 
 // add images to the hidden images div
 
